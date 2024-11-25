b70bec7f661c993564a8e9e8d02678ebad23fe77
Dhanush <draksharapudhanushsrisai.21ds@saividya.ac.in>
2024-11-25 20:30:49 +0530

    TEST URL

--- newLearning/blog-app-ts-nodejs/client/src/services/fetch/GetRequest.ts
+++ newLearning/blog-app-ts-nodejs/client/src/services/fetch/GetRequest.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { BlogType } from "../../@types/types";
 
+const URL = 'https://100xdev-lecture-assignments.vercel.app/';
+
 export const fetchBlogs = async (token: string) => {
-    const response = await axios.get("/api/v1/blog/all", {
+    const response = await axios.get(`${URL}api/v1/blog/all`, {
         headers: {
             'token': token
         }
